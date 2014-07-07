87a7b1a63244b181a550183d5a7b59ed28f93d7c
Humberto Gómez López <hoijof@ivy-corp.com>
2014-07-07 17:57:06 +0200

    stufffffff :D

--- constants.js
+++ constants.js
@@ -13,16 +13,28 @@ var objects = {
         cost     : 10,
         quantity : 0,
         rent     : 0,
-        active   : true
+        active   : true,
+        scale  : 0.15
     },
     kid : {
         name     : 'kid',
         type     : 'income',
-        value    : 10,
+        value    : 3,
         cost     : 25,
         quantity : 0,
         rent     : 1,
-        active   : false
+        active   : false,
+        scale  : 0.15
+    },
+    enslavedKid : {
+        name : 'Enslaved Kid',
+        type : 'income',
+        value : 10,
+        cost  : -1,
+        quantity : 0,
+        rent : 2,
+        active : false,
+        scale : 0
     },
     bookOfAutomation : {
         name : "Book of automation",
@@ -31,7 +43,18 @@ var objects = {
         cost : 1000,
         quantity : 0,
         rent     : 0,
-        active : false
+        active : false,
+        scale  : 0.25
+    },
+    clothes : {
+        name : "Clothes",
+        type : "equip",
+        value: 0,
+        cost : 10,
+        quantity : 1,
+        rent : 0,
+        active : true,
+        scale  : 0
     }
 };
 
@@ -42,9 +65,9 @@ var actions = {
         repercussion : -2,
         active : true,
         special : function(key) {
-            var moneyToGain = objects.kid.quantity * objects.kid.value + actions[key].value;
+            var moneyToGain = options.useChildren.checked ? objects.kid.quantity * objects.kid.value + actions[key].value : actions[key].value;
 
-            while (isAppening(10) && objects.kid.quantity) {
+            while (objects.clothes.quantity < objects.kid.quantity && (10) && objects.kid.quantity && options.useChildren.checked) {
                 addContentToTerminal("A kid was taken by the police. Those heartless bastards.");
                 objects.kid.quantity--;
             }
@@ -78,7 +101,8 @@ var actions = {
         repercussion : -6,
         active : false,
         special : function(key) {
-            var moneyToGain = objects.kid.quantity * objects.kid.value + actions[key].value;
+            var moneyToGain = options.useChildren.checked ? objects.kid.quantity * objects.kid.value + actions[key].value : actions[key].value;
+
             if (moneyToGain > baseCapacity + objects.bag.quantity * objects.bag.value) {
                 addContentToTerminal("You can't carry all the money you stole. You left " + (moneyToGain - (baseCapacity + objects.bag.quantity * objects.bag.value)) + " on the ground");
                 money += baseCapacity + objects.bag.quantity * objects.bag.value;
@@ -93,9 +117,9 @@ var actions = {
         repercussion : 1,
         active : true,
         special : function(key) {
-            var moneyToGain = objects.kid.quantity * objects.kid.value + actions[key].value;
+            var moneyToGain = options.useChildren.checked ? objects.kid.quantity * objects.kid.value + actions[key].value : actions[key].value;
 
-            while (isAppening(10) && objects.kid.quantity) {
+            while (isAppening(10) && objects.kid.quantity && options.useChildren.checked) {
                 addContentToTerminal("A kid was taken by the police. Those heartless bastards.");
                 objects.kid.quantity--;
             }
@@ -128,7 +152,27 @@ var actions = {
         repercussion : 4,
         active : false,
         special : function(key) {
-            var moneyToGain = objects.kid.quantity * objects.kid.value + actions[key].value;
+            var moneyToGain = options.useChildren.checked ? objects.kid.quantity * objects.kid.value + actions[key].value : actions[key].value;
+
+            while (isAppening(10) && objects.kid.quantity && options.useChildren.checked) {
+                addContentToTerminal("A kid was taken by the police. Those heartless bastards.");
+                objects.kid.quantity--;
+            }
+            while (isAppening(7)) {
+                addContentToTerminal("You found a kid on the street, now you'll give him a proper live.");
+                objects.kid.quantity++;
+                objects.kid.active = true;
+            }
+
+            if (isAppening(10)) {
+                addContentToTerminal("Someone sees you helping the elders and gives you two extra coin.");
+                moneyToGain += 1;
+            }
+            if (isAppening(3)) {
+                addContentToTerminal("Someone sees you helping the elders and robs you. You lose all the money you earned.");
+                moneyToGain = 0;
+            }
+
             if (moneyToGain > baseCapacity + objects.bag.quantity * objects.bag.value) {
                 addContentToTerminal("You can't carry all the money the elders gave you. You left " + (moneyToGain - (baseCapacity + objects.bag.quantity * objects.bag.value)) + " on the ground");
                 money += baseCapacity + objects.bag.quantity * objects.bag.value;
@@ -136,12 +180,38 @@ var actions = {
                 money += moneyToGain;
             }
         }
-    }};
+    },
+    giveKidToAdoption : {
+        name : 'Give kid to adoption',
+        value : 0,
+        repercussion : 10,
+        active : false,
+        special : function(key) {
+            objects.kid.quantity--;
+        }
+    },
+    enslaveKid : {
+        name : 'Enslave kid',
+        value : 0,
+        repercussion : -15,
+        active : false,
+        special : function (key) {
+            objects.kid.quantity--;
+            objects.kid.enslavedKid++;
+        }
+    }
+};
 
 var options = {
     useChildren : {
-        name   : "Use children",
-        active : false
+        name    : "Use children",
+        active  : false,
+        checked : false
+    },
+    useEnslavedKids : {
+        name : "Use enslaved kids",
+        activate : false,
+        checked : false
     }
 };
 var timeOfDayName = ['morning', 'midday', 'afternoon', 'night'];
--- main.js
+++ main.js
@@ -2,15 +2,17 @@
 var money = 25,
     karma = 0,
     day   = 0,
-    timeOfDay = 0;
+    timeOfDay = 0,
+    maxChildrenUsed = 0,
+    totalChildrenUses = 0;
 
 
 function insertObject (key) {
     var html = "";
 
-    html += "<div class='object' value='"+key+"'>"+objects[key].name+" x " + objects[key].quantity + " | " +
-        "<span class='buy'> Buy one for " + parseInt(objects[key].cost) + "</span>";
-    if (objects[key].quantity > 0) html += "<span class='sell'> | Sell one for " + parseInt(objects[key].cost*0.65) +" </span>";
+    html += "<div class='object' value='"+key+"'>"+objects[key].name+" x " + objects[key].quantity;
+    if (objects[key].cost > 0) html += " | <span class='buy'> Buy one for " + parseInt(objects[key].cost) + "</span>";
+    if (objects[key].quantity > 0 && objects[key].cost > 0) html += "<span class='sell'> | Sell one for " + parseInt(objects[key].cost*0.65) +" </span>";
 
     if (objects[key].rent > 0) html += " | Rent of " + objects[key].rent + " per day ";
     html += "</div>";
@@ -29,7 +31,12 @@ function insertAction (key) {
 function insertOption (key) {
     var html = "";
 
-    html += "<div class='action' value='"+key+"'><input type='checkbox'>"+options[key].name + "</div>";
+    if (options[key].checked) {
+        html += "<div class='action' value='"+key+"'><input type='checkbox' checked='checked'>"+options[key].name + "</div>";
+    } else {
+        html += "<div class='action' value='"+key+"'><input type='checkbox'>"+options[key].name + "</div>";
+    }
+
 
     return html;
 }
@@ -54,6 +61,27 @@ function callADay() {
     if (totalRent > 0 ) {
         addContentToTerminal("You pay a total of " + totalRent + " in maintenance costs.");
         money -= totalRent;
+    } else if (totalRent < 0) {
+        addContentToTerminal("You receive a total of " + totalRent + " in total benefits.");
+        money -= totalRent;
+    }
+}
+
+function checkActions () {
+    if (karma > -50 && karma < -15) {
+        actions.rob.active = true;
+        actions.helpElders.active = false;
+    } else if ( karma < 50 && karma > 15) {
+        actions.rob.active = false;
+        actions.helpElders.active = true;
+    }
+
+    if (objects.kid.quantity > 0) {
+        actions.enslaveKid.active = true;
+        actions.giveKidToAdoption.active = true;
+    } else {
+        actions.enslaveKid.active = false;
+        actions.giveKidToAdoption.active = false;
     }
 }
 
@@ -63,6 +91,18 @@ function checkOptions () {
     } else {
         options.useChildren.active = false;
     }
+
+    if (objects.enslavedKid.quantity > 0) {
+        options.useEnslavedKids.active = true;
+    } else {
+        options.useEnslavedKids.active = false;
+    }
+}
+
+function updateObjects () {
+    $.each(objects, function(key, value) {
+        if (value.quantity > 0) value.active = true;
+    });
 }
 
 function update () {
@@ -84,6 +124,8 @@ function update () {
     // list options
 
     checkOptions();
+    checkActions();
+
     selector = $("#options").html("");
     $.each(options, function(key, value) {
         if (options[key].active) selector.append(insertOption(key));
@@ -104,11 +146,15 @@ function update () {
 
     if ( day === 100) end();
 
+    // update children uses
+
+    maxChildrenUsed = maxChildrenUsed < objects.kid.quantity ? maxChildrenUsed = objects.kid.quantity : maxChildrenUsed;
+    if (options.useChildren.checked) totalChildrenUses += objects.kid.quantity;
 }
 
 function perform(key) {
     //money += actions[key].value;
-    //karma += actions[key].repercussion;
+    karma += actions[key].repercussion;
 
     actions[key].special(key);
 }
@@ -120,14 +166,14 @@ function buy (value, howMany) {
 
     objects[value].quantity += howMany;
     money -= objects[value].cost;
-    objects[value].cost += parseInt(objects[value].cost*0.15);
+    objects[value].cost += parseInt(objects[value].cost*objects[value].scale);
 
     update();
 }
 function sell (value, howMany) {
     objects[value].quantity -= howMany;
-    money += objects[value].cost*0.65;
-    objects[value].cost -= parseInt(objects[value].cost*0.15);
+    money += parseInt(objects[value].cost*objects[value].scale);
+    objects[value].cost -= parseInt(objects[value].cost*objects[value].scale);
 
     update();
 }
@@ -156,6 +202,12 @@ $(document).ready(function(){
         sell(key, 1);
         update();
     });
+
+    $(document).on('click', '#options div input', function() {
+        var key = $(this).parent().attr('value');
+        options[key].checked ? options[key].checked = false : options[key].checked = true;
+    });
+
 
     $(document).on('click', '#actions div', function() {
         perform($(this).attr('value'));
